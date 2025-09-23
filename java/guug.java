aaa38e4221b3b5c5bb7102332f2487c83d6c89fd
NightRaven-0 <raghavchand95@gmail.com>
2025-09-23 23:23:38 +0530

    guug

--- java/guug.java
+++ java/guug.java
@@ -1,15 +1,27 @@
-public class guug
+public class guug 
 {
     public static void main(String[] args) 
     {
-        double var1 = 1.01;
-        double var2 = 1.0001;
-        if (var1 < var2) {
-            System.out.println("1");
-        }
-        else
+        String version1 = "1.01";
+        String version2 = "1.001";
+
+        System.out.println(compareVersion(version1, version2));
+    }
+
+    public static int compareVersion(String version1, String version2) 
+    {
+        String[] v1 = version1.split("\\.");
+        String[] v2 = version2.split("\\.");
+
+        int n = Math.max(v1.length, v2.length);
+
+        for (int i = 0; i < n; i++) 
         {
-            System.out.println("-1");
+            int num1 = i < v1.length ? Integer.parseInt(v1[i]) : 0;
+            int num2 = i < v2.length ? Integer.parseInt(v2[i]) : 0;
+            if (num1 < num2) return -1;
+            if (num1 > num2) return 1;
         }
+        return 0;
     }
 }
