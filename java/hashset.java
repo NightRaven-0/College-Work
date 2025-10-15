9f6a2d3257d286f6650851dcba0fc5310536390a
NightRaven-0 <raghavchand95@gmail.com>
2025-10-15 06:25:49 +0530

    26

---
+++ java/hashset.java
@@ -0,0 +1,12 @@
+import java.util.HashSet;
+
+public class hashset {
+    public static void main(String[] args) {
+        HashSet<String> months = new HashSet<>();
+        months.add("January");
+        months.add("February");
+        months.add("March");
+        months.add("April");
+        System.out.println(months);
+    }
+}
