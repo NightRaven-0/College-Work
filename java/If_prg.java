0f25ce036790fbe1bfaa839503af7512b6a8c86a
NightRaven-0 <raghavchand95@gmail.com>
2025-10-15 06:26:20 +0530

    28

---
+++ java/If_prg.java
@@ -0,0 +1,11 @@
+public class If_prg     
+{
+    public static void main(String[] args)
+    {
+        int x = 5;
+        if (x > 10)
+            System.err.println("x is greater than 10");
+        else
+            System.err.println("x is less than or equal to 10");
+    }        
+}
